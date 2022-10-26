2e04a91d9c22b52cec9f9329976d7e37c0aafa32
ottinomarialaura <ottino.marialaura@iisgalvanimi.edu.it>
2022-10-26 11:02:39 +0000

    versione1

--- main2.py
+++ main2.py
@@ -29,3 +29,59 @@ unalista.insert(2, "anguria")
 
 print(unalista)
 
+#Per aggiungere elementi di un altro elenco all'elenco corrente, utilizzare il extend()
+unalista = ["apple", "banana", "cherry"]
+un.altra.lista = ["mango", "pineapple", "papaya"]
+
+unalista.extend(un.altra.lista)
+
+print(unalista)
+#Il remove()metodo rimuove l'elemento specificato
+unalista = ["apple", "banana", "cherry"]
+
+unalista.remove(cherry)
+
+print(unalista)
+#Se non si specifica l'indice, il pop()metodo rimuove l'ultimo elemento.
+unalista = ["apple", "banana", "cherry"]
+
+unalista.popo(2)
+
+print(unalista)
+
+#La del parola chiave può eliminare completamente l'elenco, ma anche l'indice specificato
+unalista = ["mela", "avocado", "banana", "kiwi", "melone", "mango", "papaia"]
+del unalista[1] #eliminerà solo avocado
+print(unalista) #oppure per eliminare tutta la lista:
+unalista = ["mela", "avocado", "banana", "kiwi", "melone", "mango", "papaia"]
+del unalista
+
+#Il clear()metodo svuota l'elenco. L'elenco rimane ancora, ma non avrà alcun contenuto.
+unalista = ["mela", "avocado", "banana", "kiwi", "melone", "mango", "papaia"]
+thislist.clear()
+print(unalista)
+
+#Stampa tutti gli elementi, utilizzando un whileciclo per scorrere tutti i numeri di indice
+unalista = ["mela", "avocado", "banana", "kiwi", "melone", "mango", "papaia"]
+r = 0
+while i < len(unalista):
+  print(unalista[r])
+  r = r + 1 #ora r=1 e così via
+
+#Un breve forciclo manuale che stamperà tutti gli elementi in un elenco:
+unalista = ["mela", "avocado", "banana", "kiwi", "melone", "mango", "papaia"]
+[print(x) for x in unalista]
+
+#Sulla base di un elenco, se si desidera un nuovo elenco, contenente solo elementi con la lettera "a" nel nome bisogna:
+lista1 = ["mela", "avocado", "banana", "kiwi", "melone", "mango", "papaia"]
+nuova_lista = [x for x in lista1 if "a" in x]
+
+print(nuova_lista)
+
+#La condizione if x != "apple"  ritornerà True per tutti gli elementi diversi da "mela", facendo in modo che il nuovo elenco contenga tutti i frutti tranne "mela".
+unalista = ["mela", "avocado", "banana", "kiwi", "melone", "mango", "papaia"]
+
+nuova_lista = [x for x in unalista if x != "kiwi" and "melone"]
+
+print(nuova_lista) #così verranno stampati tutti gli elementi della prima lista senza kiwi e melone
+
